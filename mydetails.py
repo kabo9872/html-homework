07715386d5c81a0f7db75497c758e2d3efec3091
kabo9872 <kabza.makachief@gmail.com>
2025-11-23 14:52:46 +0200

    Add files via upload

---
+++ mydetails.py
@@ -0,0 +1,13 @@
+
+my_details = ("Kabo", "Mosweu", 14, "1.65m", "55kg", "Mathematics")
+
+print("Tuple:", my_details)
+print()
+
+details_list = list(my_details)
+
+print("Converted to list:", details_list)
+print()
+
+for item in details_list:
+    print("Detail:", item)
